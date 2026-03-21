be1e151dc2e3e75208352863806d425b42743dd4
Sourabh Kumar <171492065+sourabhkumar21@users.noreply.github.com>
2026-03-22 03:06:48 +0530

    Fix CORS for deployment

--- server/server.ts
+++ server/server.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 /* ---------------- CORS ---------------- */
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
   })
 );
