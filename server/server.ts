6e2c0e9c8b8f1274dcb158042aaee542c1946124
Sourabh Kumar <171492065+sourabhkumar21@users.noreply.github.com>
2026-03-22 05:43:27 +0530

    fix cors for production

--- server/server.ts
+++ server/server.ts
@@ -13,7 +13,10 @@ const PORT = process.env.PORT || 5000;
 /* ---------------- CORS ---------------- */
 app.use(
   cors({
-    origin: "*",
+    origin: [
+  "http://localhost:5173", // local frontend
+  "https://studymate-ai.vercel.app" // your deployed frontend (update later if needed)
+],
     credentials: true,
   })
 );
